79e69b8bc8435e9581155bcff774f822c5921561
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:19:12 +0530

    Add project files.

---
+++ OverloadingAndOverriding/MethodOverloaing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadingAndOverriding
+{
+    public class MethodOverloaing
+    {
+        public int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Add(int a,int b)
+        {
+            return a + b;
+
+        }
+    }
+}
---
+++ OverloadingAndOverriding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OverloadingAndOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOverloaing ab = new MethodOverloaing();
+            int num1=ab.Add(5, 6, 2);
+            int num2=ab.Add(78, 56);
+            Console.WriteLine("the addition of first method  :" + num1);
+            Console.WriteLine("the addition of second method : " + num2);
+        }
+    }
+}
